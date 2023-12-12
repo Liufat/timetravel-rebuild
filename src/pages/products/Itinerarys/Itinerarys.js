6b26bb06bae45a370ffa8292693a866ae71dce07
Liufat <dd5672003@gmail.com>
2023-12-12 17:58:40 +0800

    [初始化]

---
+++ src/pages/products/Itinerarys/Itinerarys.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Itinerarys() {
+  return <div>Itinerarys</div>;
+}
+
+export default Itinerarys;
---
+++ src/pages/products/Stays/Stays.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Stays() {
+  return <div>Stays</div>;
+}
+
+export default Stays;
