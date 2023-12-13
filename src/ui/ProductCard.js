0eca407108d29f2f120b2907b55c42ec0dc7b35b
Liufat <dd5672003@gmail.com>
2023-12-13 17:58:33 +0800

    [新增]ui-productCard

---
+++ src/ui/ProductCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { FaStar } from "react-icons/fa";
+import styled from "styled-components";
+
+const StyledStar = styled(FaStar)`
+  fill: var(--color-primary);
+`;
+
+const StyledSpan = styled.span`
+  color: var(--color-primary);
+  font-weight: 700;
+`;
+
+function ProductCard({ className, img, title, star, price }) {
+  return (
+    <div className={`${className} boxShadow`}>
+      <img className="w-100" alt="banner" src={img} />
+      <div className="d-flex flex-column gap-5  p-3">
+        <div className="d-flex flex-column justify-content-around">
+          <span>[即買即用]</span>
+          <span>{title}</span>
+          <div className="d-flex align-items-center gap-1">
+            <StyledStar />
+            <span>{star}</span>
+          </div>
+        </div>
+
+        <StyledSpan className="text-end">NT${price}</StyledSpan>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
