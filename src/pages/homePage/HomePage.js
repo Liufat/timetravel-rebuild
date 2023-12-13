2add21d71f23bf0d64cfe107a168e905f87cb5c7
Liufat <dd5672003@gmail.com>
2023-12-13 14:07:22 +0800

    [新增]背景圖片

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import Header from "../../ui/Header";
+import Footer from "../../ui/Footer";
+import styled from "styled-components";
+import backGroundImage from "./../../image/img/background.png";
+import Banner from "./sections/Banner";
+
+const StyledBackGround = styled.div`
+  background-image: url(${backGroundImage});
+  background-position: center;
+  background-repeat: no-repeat;
+`;
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <StyledBackGround>
+        <Header />
+        <Banner />
+      </StyledBackGround>
+      <main>
+        <section>discount</section>
+        <section>hot product</section>
+        <section>round image</section>
+        <section>why you choose tt</section>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default HomePage;
