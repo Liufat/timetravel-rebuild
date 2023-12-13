e3fc63a0668fe98e311123a22d898dadb0c76171
Liufat <dd5672003@gmail.com>
2023-12-13 14:07:59 +0800

    [新增]Header的樣式

---
+++ src/ui/Header.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "./../image/icon/logo/logo_white.svg";
+import { NavLink } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
+import { CiSearch } from "react-icons/ci";
+
+const StyledLink = styled(NavLink)`
+  color: var(--color-white);
+`;
+
+const StyledCartIcon = styled(FiShoppingCart)`
+  color: var(--color-white);
+  height: 100%;
+  width: 1.5rem;
+`;
+
+const StyledSearchWrap = styled.div`
+  position: relative;
+`;
+
+const StyledSearchIcon = styled(CiSearch)`
+  position: absolute;
+  height: 100%;
+  width: 1.5rem;
+  left: 0.5rem;
+  top: 0;
+  bottom: 0;
+`;
+
+const StyledSearchInput = styled.input`
+  background-color: transparent;
+  border: 1px solid var(--color-white);
+  border-radius: 30px;
+  color: var(--color-white);
+  &::placeholder {
+    color: var(--color-white);
+    padding-left: 2.5rem;
+  }
+`;
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="row py-4">
+        <div className="d-flex justify-content-between aling-items-center">
+          <div className="d-flex col-5 justify-content-between align-items-center">
+            <img className="col-3" alt="logo" src={logo} />
+            <StyledLink href="/">行程</StyledLink>
+            <StyledLink href="/">美食</StyledLink>
+            <StyledLink href="/">住宿</StyledLink>
+            <StyledLink href="/">票券</StyledLink>
+          </div>
+          <div className="d-flex col-5 justify-content-between">
+            <StyledSearchWrap>
+              <StyledSearchIcon />
+              <StyledSearchInput type="text" placeholder="搜尋" />
+            </StyledSearchWrap>
+            <StyledLink href="/">登入</StyledLink>
+            <StyledLink href="/">註冊</StyledLink>
+            <StyledCartIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
