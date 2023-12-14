db9c1fe1cff9e40c126c414827fb5eb6074e8b4b
Liufat <dd5672003@gmail.com>
2023-12-14 15:26:21 +0800

    [新增]頁面樣式

---
+++ src/pages/homePage/sections/WhyChoose.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import backGroundImg from "./../../../image/img/background2.png";
+import Button from "../../../ui/Button";
+
+const StyledSection = styled.section`
+  height: 30rem;
+  background-image: url(${backGroundImg});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const StyledH1 = styled.h1`
+  color: var(--color-white);
+`;
+const StyledH2 = styled.h2`
+  color: var(--color-white);
+`;
+function WhyChoose() {
+  return (
+    <StyledSection className="d-flex align-items-center justify-content-center">
+      <div className="d-flex flex-column gap-5 align-items-center">
+        <StyledH1>為什麼選擇TimeTravel？</StyledH1>
+        <div className="d-flex flex-column align-items-center">
+          <StyledH2>
+            你知道世界上有超過百萬名自由行旅客選擇TimeTravel作為旅遊規劃網站嗎？
+          </StyledH2>
+          <StyledH2>他們透過TimeTravel找到安全又安心的體驗行程！</StyledH2>
+        </div>
+        <div>
+          <Button type="transparent" className={"py-1 px-3"}>
+            <span>立即註冊</span>
+          </Button>
+        </div>
+      </div>
+    </StyledSection>
+  );
+}
+
+export default WhyChoose;
--- src/ui/Footer.js
+++ src/ui/Footer.js
@@ -1,7 +1,51 @@
 import React from "react";
+import styled from "styled-components";
+import footerLogo from "./../image/icon/logo/logo_white.svg";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const StyledFooter = styled.footer`
+  height: 30rem;
+  background-color: var(--color-grey);
+`;
+
+const StyledBorder = styled.div`
+  padding-bottom: 3rem;
+  border-bottom: 1px solid var(--color-white);
+`;
+
+const StyledIconWrap = styled.div`
+  color: var(--color-white);
+`;
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <StyledFooter className="d-flex justify-content-center align-items-center">
+      <div className="col-xl-8">
+        <StyledBorder className="d-flex justify-content-between ">
+          <div className="col-xl-2">
+            <img alt="logo" className="w-100" src={footerLogo}></img>
+          </div>
+          <StyledIconWrap className="col-xl-1 d-flex justify-content-center gap-5">
+            <h1>
+              <FaFacebookF />
+            </h1>
+            <h1>
+              <FaInstagram />
+            </h1>
+          </StyledIconWrap>
+        </StyledBorder>
+        <StyledIconWrap className="d-flex justify-content-between py-5 ">
+          <div>
+            <span>E-Mail：service@timetravel.com</span>
+          </div>
+          <div>
+            <span>COPYRIGHT &copy; 2022 TimeTravel All rights reserved</span>
+          </div>
+        </StyledIconWrap>
+      </div>
+    </StyledFooter>
+  );
 }
 
 export default Footer;
