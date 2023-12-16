6b66f0879ae0d85586ba435b60ca74370ede63f2
Liufat <dd5672003@gmail.com>
2023-12-17 00:49:26 +0800

    [新增]切版

--- src/pages/products/Foods/FoodDetail.js
+++ src/pages/products/Foods/FoodDetail.js
@@ -11,6 +11,7 @@ import { IoFastFoodOutline } from "react-icons/io5";
 import { IoPhonePortraitOutline } from "react-icons/io5";
 import { FaMinusCircle } from "react-icons/fa";
 import { FaCirclePlus } from "react-icons/fa6";
+import Button from "../../../ui/Button";
 
 const StyledProductBanner = styled.div`
   width: 100%;
@@ -49,46 +50,66 @@ function FoodDetail() {
         </h2>
       </section>
       <section className="d-flex justify-content-between align-items-center">
-        <div className="d-flex col-xl-9 justify-content-between">
-          <div className="d-flex gap-5">
-            <div className="d-flex">
-              <h2>
-                <FiMapPin />
-              </h2>
-              <h2 className="pt-1">三重區</h2>
+        <div className="d-flex flex-column col-xl-9 gap-5">
+          <div className="d-flex justify-content-between">
+            <div className="d-flex gap-5">
+              <div className="d-flex">
+                <h2>
+                  <FiMapPin />
+                </h2>
+                <h2 className="pt-1">三重區</h2>
+              </div>
+              <div className="d-flex">
+                <h2>
+                  <IoFastFoodOutline />
+                </h2>
+                <h2 className="pt-1">飲品</h2>
+              </div>
             </div>
-            <div className="d-flex">
+            <div className="d-flex align-items-center">
               <h2>
-                <IoFastFoodOutline />
+                <IoPhonePortraitOutline />
               </h2>
-              <h2 className="pt-1">飲品</h2>
+              <span className="pt-1">現場請出示電子票券</span>
             </div>
           </div>
-          <div className="d-flex align-items-center">
-            <h2>
-              <IoPhonePortraitOutline />
-            </h2>
-            <span className="pt-1">現場請出示電子票券</span>
+          <p>
+            50嵐自1994年創立品牌，這些年來我們堅持只賣茶飲，用精選茶葉，搭配好的原料，新鮮現調每一杯茶飲。感謝每一位支持50嵐的客人一直以來對我們的愛護，我們將更努力，希望能提供更好的商品與服務給每一位客人。
+          </p>
+
+          <div>
+            <h2>商品介紹</h2>
+            
           </div>
         </div>
-        <div className="col-xl-2">
-          <h2>選擇張數</h2>
-          <div className="d-flex align-items-center gap-5">
-            <h2>
-              <FaMinusCircle />
+
+        <div className="col-xl-2 d-flex flex-column gap-3">
+          <div className="d-flex flex-column gap-2">
+            <h2>選擇張數</h2>
+            <div className="d-flex align-items-center gap-5">
+              <h2>
+                <FaMinusCircle />
+              </h2>
+              <h2 className="pt-1">1</h2>
+              <h2>
+                <FaCirclePlus className="text-color-primary" />
+              </h2>
+            </div>
+          </div>
+          <div>
+            <h2 className="text-color-grey">
+              <del>TWD$899</del>
             </h2>
-            <h2 className="pt-1">1</h2>
-            <h2>
-              <FaCirclePlus className="text-color-primary" />
-            </h2>
+            <h1 className="text-color-primary">TWD$599</h1>
+          </div>
+          <div className="d-flex gap-3">
+            <Button className="col-xl-6 btn-secondary py-2 px-2 ">
+              加入購物車
+            </Button>
+            <Button className="col-xl-6 btn-primary py-2 px-2">立即購買</Button>
           </div>
         </div>
       </section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
     </main>
   );
 }
