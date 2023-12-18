a4971d352856f9bae8738258c6595225dbefd3e2
Liufat <dd5672003@gmail.com>
2023-12-18 21:29:37 +0800

    [修改]輪播牆RWD

--- src/pages/homePage/sections/RoundImage.js
+++ src/pages/homePage/sections/RoundImage.js
@@ -1,50 +1,55 @@
 import React from "react";
 import pic1 from "./../../../image/img/itinerary_home.jpg";
-import styled from "styled-components";
-import RoundPictureCard from "../../../ui/RoundPictureCard";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
-const StyledSpan = styled.span`
-  cursor: pointer;
-`;
+import RoundPictureCard from "../../../ui/RoundPictureCard";
+
+import Slider from "react-slick";
+import NextArrow from "../../../ui/NextArrow";
+import PreArrow from "../../../ui/PreArrow";
+const settings = {
+  arrows: true,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  nextArrow: <NextArrow />,
+  prevArrow: <PreArrow />,
+  responsive: [
+    {
+      breakpoint: 1030,
+      settings: {
+        slidesToShow: 5,
+      },
+    },
+    {
+      breakpoint: 770,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 430,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+};
 
 function RoundImage() {
   return (
-    <section className="d-flex py-5 justify-content-center">
-      <div className="d-flex py-5 col-xl-9 justify-content-evenly gap-5 gap-xl-0">
-        <StyledSpan className="d-flex d-xl-none align-items-center">
-          <FaChevronLeft />
-        </StyledSpan>
-        <RoundPictureCard pic={pic1} className="d-flex justify-content-center">
-          防疫旅館
-        </RoundPictureCard>
-        <RoundPictureCard
-          pic={pic1}
-          className="d-none d-md-flex justify-content-center"
-        >
-          防疫旅館
-        </RoundPictureCard>
-        <RoundPictureCard
-          pic={pic1}
-          className="d-none d-md-flex justify-content-center"
-        >
-          防疫旅館
-        </RoundPictureCard>
-        <RoundPictureCard
-          pic={pic1}
-          className="d-none d-lg-flex justify-content-center"
-        >
-          防疫旅館
-        </RoundPictureCard>
-        <RoundPictureCard
-          pic={pic1}
-          className="d-none d-xl-flex justify-content-center"
-        >
-          防疫旅館
-        </RoundPictureCard>
-        <StyledSpan className="d-flex d-xl-none align-items-center">
-          <FaChevronRight />
-        </StyledSpan>
+    <section className="w-100 py-5">
+      <div className="d-flex w-100 justify-content-center">
+        <div className="col-9 col-md-10">
+          <Slider {...settings}>
+            <RoundPictureCard pic={pic1}>防疫旅館</RoundPictureCard>
+            <RoundPictureCard pic={pic1}>防疫旅館</RoundPictureCard>
+            <RoundPictureCard pic={pic1}>防疫旅館</RoundPictureCard>
+            <RoundPictureCard pic={pic1}>防疫旅館</RoundPictureCard>
+            <RoundPictureCard pic={pic1}>防疫旅館</RoundPictureCard>
+          </Slider>
+        </div>
       </div>
     </section>
   );
