63151adc0191233773ca0c0737b16fac3485376c
Liufat <dd5672003@gmail.com>
2023-12-18 21:04:26 +0800

    [修改]輪播牆

--- src/pages/homePage/sections/HotProduct.js
+++ src/pages/homePage/sections/HotProduct.js
@@ -3,58 +3,97 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import styled from "styled-components";
 import ProductCard from "../../../ui/ProductCard";
 import pic1 from "./../../../image/img/hotpot_home.jpeg";
-
-const StyledSpan = styled.span`
-  cursor: pointer;
-`;
+import Slider from "react-slick";
+import NextArrow from "../../../ui/NextArrow";
+import PreArrow from "../../../ui/PreArrow";
 
 function HotProduct() {
+  const settings = {
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PreArrow />,
+    responsive: [
+      {
+        breakpoint: 1030,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 430,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="d-flex flex-column align-items-center ">
       <h2 className="py-5">熱門商品</h2>
-      <div className="d-flex col-10 col-xl-10">
-        <div className="d-flex justify-content-center align-items-center gap-4">
-          <StyledSpan>
-            <FaChevronLeft />
-          </StyledSpan>
-          <ProductCard
-            className={"col-xl-2"}
-            img={pic1}
-            title="台北兒童新樂園 | 一日票"
-            star="4.9/5"
-            price="499"
-          />
-          <ProductCard
-            className={"d-none d-md-block col-xl-2"}
-            img={pic1}
-            title="台北兒童新樂園 | 一日票"
-            star="4.9/5"
-            price="499"
-          />
-          <ProductCard
-            className={"d-none d-lg-block col-xl-2"}
-            img={pic1}
-            title="台北兒童新樂園 | 一日票"
-            star="4.9/5"
-            price="499"
-          />
-          <ProductCard
-            className={"d-none d-xl-block col-xl-2"}
-            img={pic1}
-            title="台北兒童新樂園 | 一日票"
-            star="4.9/5"
-            price="499"
-          />
-          <ProductCard
-            className={"d-none d-xl-block col-xl-2"}
-            img={pic1}
-            title="台北兒童新樂園 | 一日票"
-            star="4.9/5"
-            price="499"
-          />
-          <StyledSpan>
-            <FaChevronRight />
-          </StyledSpan>
+      <div className="d-flex justify-content-center w-100">
+        <div className="col-10">
+          <Slider {...settings}>
+            <ProductCard
+              img={pic1}
+              title="台北兒童新樂園 | 一日票"
+              star="4.9/5"
+              price="499"
+            />
+            <ProductCard
+              img={pic1}
+              title="台北兒童新樂園 | 一日票"
+              star="4.9/5"
+              price="499"
+            />
+            <ProductCard
+              img={pic1}
+              title="台北兒童新樂園 | 一日票"
+              star="4.9/5"
+              price="499"
+            />
+            <ProductCard
+              img={pic1}
+              title="台北兒童新樂園 | 一日票"
+              star="4.9/5"
+              price="499"
+            />
+            <ProductCard
+              img={pic1}
+              title="台北兒童新樂園 | 一日票"
+              star="4.9/5"
+              price="499"
+            />
+            <ProductCard
+              img={pic1}
+              title="台北兒童新樂園 | 一日票"
+              star="4.9/5"
+              price="499"
+            />
+            <ProductCard
+              img={pic1}
+              title="台北兒童新樂園 | 一日票"
+              star="4.9/5"
+              price="499"
+            />
+            <ProductCard
+              img={pic1}
+              title="台北兒童新樂園 | 一日票"
+              star="4.9/5"
+              price="499"
+            />
+          </Slider>
         </div>
       </div>
     </section>
--- src/ui/ProductCard.js
+++ src/ui/ProductCard.js
@@ -17,7 +17,7 @@ function ProductCard({ className, img, title, star, price }) {
     <div className={`${className} box-shadow`}>
       <img className="w-100" alt="banner" src={img} />
       <div className="d-flex flex-column gap-5  p-3">
-        <div className="d-flex flex-column justify-content-around">
+        <div className="d-flex flex-column align-items-start justify-content-around">
           <span>[即買即用]</span>
           <span>{title}</span>
           <div className="d-flex align-items-center gap-1">
