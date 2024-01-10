055f1d024962e156e76a9145a4e79d40264c67be
Liufat <dd5672003@gmail.com>
2024-01-10 17:22:00 +0800

    [新增]頁面

--- src/pages/cart/CartTicket.js
+++ src/pages/cart/CartTicket.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CartProductCard from "../../ui/CartProductCard";
+import { useCart } from "../../context/CartContext";
 
 function CartTicket() {
-  return <div>CartTicket</div>;
+  const { cartState } = useCart();
+  const [cartTicket, setCartTicket] = useState([]);
+  useEffect(() => {
+    const cartTicketObject = cartState.cartItems.filter((item) => {
+      return item.type === "ticket";
+    });
+    setCartTicket(cartTicketObject);
+  }, [cartState.cartItems]);
+  const createDom = () =>
+    cartTicket.map((v) => {
+      return <CartProductCard key={v.id} type={"ticket"} item={v} />;
+    });
+
+  return <>{createDom()}</>;
 }
 
 export default CartTicket;
