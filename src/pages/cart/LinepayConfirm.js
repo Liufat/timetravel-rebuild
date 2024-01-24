a92178fed837af6c34a2a0f8236c2a9ec4626a32
Liufat <dd5672003@gmail.com>
2024-01-24 14:26:12 +0800

    [修改]解決頁面會兩次render引起連續呼喚line pay confirm api造成的錯誤回報

--- src/pages/cart/LinepayConfirm.js
+++ src/pages/cart/LinepayConfirm.js
@@ -2,20 +2,24 @@ import queryString from "query-string";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { LINE_PAY_CONFIRM_API } from "../../server/config";
+import { useEffect } from "react";
 
 function LinepayConfirm() {
   const query = queryString.parse(useLocation().search);
 
-  const checkPay = async () => {
-    const result = await axios.post(LINE_PAY_CONFIRM_API, query);
-    if (result.data === "success") {
-      window.location = "http://localhost:3000/success";
-    } else {
-      window.location = "http://localhost:3000/fail";
-    }
-  };
+  useEffect(() => {
+    const checkPay = async () => {
+      const result = await axios.post(LINE_PAY_CONFIRM_API, query);
+      console.log(result);
+      if (result.data === "success") {
+        window.location = "http://localhost:3000/success";
+      } else {
+        window.location = "http://localhost:3000/fail";
+      }
+    };
 
-  checkPay();
+    checkPay();
+  }, [query]);
 
   //   console.log(query);
 
